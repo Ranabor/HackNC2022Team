e95b782c899a04c0d68d9a6d737b491bcdd4b18f
yashusi <yashusi@unc.edu>
2022-11-05 22:22:42 -0400

    text_file_reader progress

--- dpci_lookup.py
+++ dpci_lookup.py
@@ -1,16 +1,14 @@
 import requests
 import json
 
-# set up the request parameters
-params = {
-  'api_key': '6C7B23038BF54E3AAB67F70E03EBBA13',
-  'type': 'product',
-  'dpci': '071180218'
-}
-
-# make the http GET request to RedCircle API
-api_result = requests.get('https://api.redcircleapi.com/request', params)
-
-# print the JSON response for price
-#print(json.dumps(api_result.json()["product"]["buybox_winner"]["price"]["value"]))
-print(api_result.json()["product"]["buybox_winner"]["price"]["value"])
+def dpci_title_lookup(dpci: str) -> str:
+  # set up the request parameters
+  params = {
+    'api_key': '6C7B23038BF54E3AAB67F70E03EBBA13',
+    'type': 'product',
+    'dpci': dpci
+  }
+  # make the http GET request to RedCircle API
+  api_result = requests.get('https://api.redcircleapi.com/request', params)
+  return api_result.json()["product"]["title"]
+  # print the JSON response for price
