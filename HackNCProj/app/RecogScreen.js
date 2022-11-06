404763b82df591d857f0eb098407edbc5d42d696
Rishi Reddy <ranabor@unc.edu>
2022-11-05 23:24:32 -0400

    Recognize Screen to Class Async Storage

--- HackNCProj/app/RecogScreen.js
+++ HackNCProj/app/RecogScreen.js
@@ -1,28 +1,57 @@
-import React, {useState, useEffect} from 'react';
-import {Button, StyleSheet, Text, View, Image} from 'react-native';
+import React, {useState, useEffect, Component} from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  PermissionsAndroid,
+} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import TextRecognition from 'react-native-text-recognition';
 import {launchImageLibrary} from 'react-native-image-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const RecogScreen = () => {
-  const [image, setImage] = useState(null);
-  const [text, setText] = useState(null);
-  useEffect(() => {
-    launchImageLibrary({}, setImage);
-  }, []);
+class RecogScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+      text: '',
+    };
+  }
+  componentDidMount() {}
 
-  useEffect(() => {
-    (async () => {
-      if (image) {
-        const result = await TextRecognition.recognize(image.assets[0].uri);
-        setText(result);
-      } else {
-        setText('No image chosen.');
-      }
-    })();
-  }, [image]);
+  upload;
 
-  return <View>{text ? <Text>{text}</Text> : null}</View>;
-};
+  render() {
+    return (
+      <View>
+        <Text>A</Text> {this.state.text ? <Text>{this.state.text}</Text> : null}
+      </View>
+    );
+  }
+}
+
+// const RecogScreen = () => {
+//   const [image, setImage] = useState(null);
+//   const [text, setText] = useState(null);
+//   useEffect(() => {
+//     launchImageLibrary({}, setImage);
+//   }, []);
+
+//   useEffect(() => {
+//     (async () => {
+//       if (image) {
+//         const result = await TextRecognition.recognize(image.assets[0].uri);
+//         setText(result);
+//       } else {
+//         setText('No image chosen.');
+//       }
+//     })();
+//   }, [image]);
+
+//   return <View>{text ? <Text>{text}</Text> : null}</View>;
+// };
 
 export default RecogScreen;
