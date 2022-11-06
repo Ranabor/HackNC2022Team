ac1b43c8f3c523916b72d6337f960eff5f9f6532
nsivaku <nithin.sivakumaran@gmail.com>
2022-11-06 10:45:21 -0500

    Icon removed from Nav Bar

--- HackNCProj/navigation/TabNavigator/index.js
+++ HackNCProj/navigation/TabNavigator/index.js
@@ -10,7 +10,11 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Navigator screenOptions={{headerShown: false, tabBarIconStyle: { display: "none" },tabBarLabelStyle: {
+      fontWeight: "700",
+      fontSize: 28,
+      alignSelf: 'center',
+    }}}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Scanner" component={RecogScreen} />
       <Tab.Screen name="History" component={History} />
